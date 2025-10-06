bc05cf1ee1162cc9659149e3af5cc7aedd443bf6
hasan olleik <hsnlk1583@gmail.com>
2025-10-06 21:54:04 +0300

    added database pw

--- src/config/db.js
+++ src/config/db.js
@@ -1,10 +1,13 @@
 const mysql = require("mysql2");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "mindhive_db",
+ host: process.env.DB_HOST,
+ user: process.env.DB_USER,
+ password: process.env.DB_PASSWORD,
+ database: process.env.DB_NAME
 });
 
 connection.connect((err) => {
