ce339672a202c8fdc1736b66aade1e0a3f0f29be
Tarek Khatib <tarekalkhatibb@gmail.com>
2025-12-01 12:14:03 +0200

    cache control

--- app.js
+++ app.js
@@ -19,6 +19,13 @@ dotenv.config();
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.setHeader("Cache-Control", "no-store, no-cache, must-revalidate, proxy-revalidate");
+  res.setHeader("Pragma", "no-cache");
+  res.setHeader("Expires", "0");
+  next();
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
